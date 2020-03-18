564a23a6c28499df47ee4c7585c7558647978ad0
Johnny_Wick <wolfmib@gmail.com>
2020-03-18 10:55:44 +0800

    save_replace_string_python_in-build-example

---
+++ replace_string.py
@@ -0,0 +1,15 @@
+
+
+
+if __name__ == "__main__":
+
+    msg  = "[Jason]: For replace the string \n"
+    msg += "       : 1 loop each c and find pattern to replace,                 (slow) 😓\n"
+    msg += "       : 2 use regular expression package 're                       (slow) 😓\n"
+    msg += "       : 3 use python(in-build) string.replace('pattern','replace') faster ✅\n"
+
+    head_line = "-----------------------------------------------------------------------"
+    print(head_line)
+    print(msg)
+    print(head_line)
+
