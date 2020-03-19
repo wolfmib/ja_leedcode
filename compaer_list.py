aba12d7e7d92a43768e1145ebb6fdb23257dccfa
Johnny_Wick <wolfmib@gmail.com>
2020-03-19 12:11:17 +0800

    add_example

---
+++ compaer_list.py
@@ -0,0 +1,10 @@
+
+my_list = [1,2,3,4,5]
+an_list = [1,2,3,4,5]
+
+print("my_list          =",my_list)
+print("an_list          =",an_list)
+print("-------------------------------")
+if my_list == an_list:
+    print("The are the same")
+
---
+++ sort.py
@@ -0,0 +1,11 @@
+
+my_list = [7,8,2,4,3,2,1]
+print("list                     =",my_list)
+my_list.sort()
+print("list.sort()              =",my_list)
+
+
+my_list = [7,8,2,4,3,2,1]
+print("list                     =",my_list)
+my_list.sort(reverse=True)
+print("list.sort(reverse=True)  =",my_list)
